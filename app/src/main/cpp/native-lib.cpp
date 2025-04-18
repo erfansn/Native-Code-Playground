f1cd5a4d196547557d00155ecbb210682b3f481e
Erfan Sadigh Nejati <erfansn.es@gmail.com>
2025-04-18 14:11:58 +0330

    Implement two function to show difference between jni only and jni bind

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,9 +1,51 @@
 #include <jni.h>
 #include <string>
+#include "jni_bind.h"
 
 extern "C"
 JNIEXPORT jstring JNICALL
-Java_ir_erfansn_nativecodeplayground_MainActivity_stringFromJNI(JNIEnv *env, jobject thiz) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+Java_ir_erfansn_nativecodeplayground_MainActivity_concatenateMembersNameJni(JNIEnv *env, jobject thiz, jobject company) {
+    std::string names;
+
+    auto companyClass = env->FindClass("ir/erfansn/nativecodeplayground/Company");
+    auto staffsFieldId = env->GetFieldID(companyClass, "staffs", "Ljava/util/List;");
+    auto staffsJObject = env->GetObjectField(company, staffsFieldId);
+    auto listClass = env->FindClass("java/util/List");
+    auto listSizeMethodId = env->GetMethodID(listClass, "size", "()I");
+    auto listSize = env->CallIntMethod(staffsJObject, listSizeMethodId);
+    auto listGetMethodId = env->GetMethodID(listClass, "get", "(I)Ljava/lang/Object;");
+    for (int i = 0; i < listSize; ++i) {
+        auto staff = (jstring) env->CallObjectMethod(staffsJObject, listGetMethodId, i);
+        names += env->GetStringUTFChars(staff, nullptr);
+    }
+
+    return env->NewStringUTF(names.c_str());
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_ir_erfansn_nativecodeplayground_MainActivity_concatenateMembersNameJniBind(JNIEnv *env, jobject thiz, jobject company) {
+    std::string names;
+
+    auto jvm{std::make_unique<jni::JvmRef<jni::kDefaultJvm>>(env)};
+
+    constexpr jni::Class jList {
+            "java/util/List∑œ",
+            jni::Method{"get", jni::Return{jni::Class{"java/lang/Object"}}, jni::Params<jint>{}},
+            jni::Method{"size", jni::Return<jint>{}},
+    };
+    static constexpr jni::Class jCompany {
+            "ir/erfansn/nativecodeplayground/Company",
+            jni::Field{"staffs", jList}
+    };
+
+    auto companyWrapper = jni::LocalObject<jCompany>{company};
+    auto staffs = companyWrapper.Access<"staffs">().Get();
+    for (int i = 0; i < staffs.Call<"size">(); ++i) {
+        auto nameObject = staffs.Call<"get">(i);
+        auto name = jni::LocalString{nameObject.Release()};
+        names += std::string(name.Pin().ToString());
+    }
+
+    return env->NewStringUTF(names.c_str());
 }
--- app/src/main/java/ir/erfansn/nativecodeplayground/MainActivity.kt
+++ app/src/main/java/ir/erfansn/nativecodeplayground/MainActivity.kt
@@ -4,32 +4,54 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ir.erfansn.nativecodeplayground.ui.theme.NativeCodePlaygroundTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val company = Company(listOf("Erfan", "Ali", "Reza"))
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             NativeCodePlaygroundTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        Text("With JNI:")
+                        Greeting(
+                            name = concatenateMembersNameJni(company),
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Text("With JNI Bind:")
+                        Greeting(
+                            name = concatenateMembersNameJniBind(company),
+                        )
+                    }
                 }
             }
         }
     }
 
-    external fun stringFromJNI(): String
+    private external fun concatenateMembersNameJni(company: Company): String
+
+    private external fun concatenateMembersNameJniBind(company: Company): String
+
+    companion object {
+        init {
+            System.loadLibrary("nativecodeplayground")
+        }
+    }
 }
 
 @Composable
